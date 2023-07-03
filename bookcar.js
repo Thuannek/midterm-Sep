85f8cb3a1e26bd40f6c70566aeeb9d0a2bd1df82
Thuannek <thuan.le210219@vnuk.edu.vn>
2023-07-03 21:24:11 +0700

    xoá

--- bookcar.js
+++
@@ -1,8 +0,0 @@
-
-var date = new Date();
-document.getElementById("year").innerHTML = date;
-
-function Copyright() {
-    var year = date.getFullYear();
-    document.getElementById("year").innerHTML = year;
-}
