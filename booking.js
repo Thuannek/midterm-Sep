d057a074838f6c77517e3f33a6c0784a2a2565af
Thuannek <thuan.le210219@vnuk.edu.vn>
2023-07-12 16:14:06 +0700

    update2

---
+++ booking.js
@@ -0,0 +1,19 @@
+function booking_hotel1(){
+    location.assign("booking.html");
+}
+function booking_hotel2(){
+    location.assign("booking2.html");
+}
+function booking_hotel3(){
+    location.assign("booking3.html");
+}
+function booking_hotel4(){
+    location.assign("booking4.html");
+}
+function booking_hotel5(){
+    location.assign("booking5.html");
+}
+function booking_hotel6(){
+    location.assign("booking6.html");
+}
+
--- hotel.js
+++ hotel.js
@@ -1,29 +1,27 @@
-// https://console.cloud.google.com  but fixed it all/
-(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
-  key: "AIzaSyA4jXioOnAqSCZxy9y1Zl8udMK6EkvmDYM",
-});
-let map;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 16.07104362794702, lng: 108.22028342511129 },
-    zoom: 100,
-  });
+function dieu_huong1(){
+  location.assign("detail-hotel1.html");
 }
-initMap();
-
-
-// button 
-// document.querySelectorAll('.detail').forEach(element => {
-//  element.addEventListener('click',function(){
-//   alert('ERROR');
-// });
-// });
-function dieu_huong(){
-  location.assign("index.html");
+function dieu_huong2(){
+  location.assign("detail-hotel2.html");
 }
+
+function dieu_huong3(){
+  location.assign("detail-hotel3.html");
+}
+
+function dieu_huong4(){
+  location.assign("detail-hotel4.html");
+}
+
+function dieu_huong5(){
+  location.assign("detail-hotel5.html");
+}
+
+function dieu_huong6(){
+  location.assign("detail-hotel6.html");
+}
+
 
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
